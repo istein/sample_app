6190b9f6d668df4f8ad0b81bb731dbe409c553b3
Isaac Stein <istein@mediamath.com>
2014-06-24 16:00:14 -0400

    Added user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -17,4 +17,18 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   
   default_scope order: 'microposts.created_at DESC' #DESC is SQL for “descending”
+
+  # Returns microposts from the users being followed by the given user.
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: user.id)
+  end
+
+	# def self.from_users_followed_by(user)
+	# 	followed_user_ids = user.followed_user_ids
+	# 	where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+	# end
+
 end
